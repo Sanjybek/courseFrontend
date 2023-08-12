7e3de04e9880d2bbf919d04b0cf7992bd030e685
Sanjybek <sanjybek003@gmail.com>
2023-08-12 20:08:16 +0600

    lesson-1

---
+++ LSN_1/lesson/main.js
@@ -0,0 +1,105 @@
+// console.log('hello world'); //это функция выведит нам какоето сообщение
+// alert('Уведомление в окне'); 
+
+// confirm(' Принять условия');
+// prompt('Ввыедитье ваше имя');
+
+// var age = 30;    //Устаревшый 
+// console.log(age);
+
+let name = 'Sasha';
+const age = 50;
+
+console.log(name, age);
+// age = 30 //не можем переопределить const
+// name = 'Max'
+// console.log(name, age);
+
+// let firstName = prompt('Ввыедитье ваше имя');
+// console.log(firstName);
+
+// let isActiv = confirm('авторизоватся')
+// console.log(isActiv);
+
+// let string = 'строка'; //строка
+// let number = 200; //нумеровый
+// let boolean1 = true; //булевый типы данных
+// let boolean2 = false;
+
+// let und = undefined; // кагда переменный ничего присвоена
+// let pp
+// console.log(pp);
+
+// let n = null; //это назночение null - то есть пустое
+
+// let obj = {
+//     name: 'max',
+//     age: 30,
+//     main: {
+//         first: 'alex',
+//         age: 60,
+//     }
+// } //хранить ключи и значение люьых типы
+
+// let arr = [1 , 'kdcnf', {} , ovj, true] //массив список значении
+
+//TODO операторы
+// let sum = 2 + 2 //сложение
+// console.log(sum);
+
+// let differense =  5 - 3; //вычитание 
+// console.log(differense); 
+
+// let product  = 4 * 3; //умножение 
+// console.log(product);
+
+// let guontiend = 10 / 2 ;//деление
+// console.log(guontiend);
+
+// let remainder = 15 % 3; //возврашает остаток делении
+// console.log(remainder);
+
+// let increment = 7;
+// console.log(increment);
+// increment++; //добовляет один
+// console.log(increment);
+
+// let decrement = 6;
+// console.log(decrement);
+// decrement--;
+// console.log(decrement);
+
+// let x = 10
+
+// x = x + 5
+// x = x * 5
+// x = x - 5
+// x = x / 5
+
+// x += 5 //укороченный версия
+// x -= 5
+// x /= 5
+// x *= 5
+// console.log(x);
+
+//Todo
+let TextNum = '120'
+let numNum = 2
+let sumText = TextNum + numNum // 
+console.log(sumText);
+
+TextNum = '120'
+numNum = 2
+sumText = TextNum - numNum // - минус будет действовать
+console.log(sumText);
+
+TextNum = '120n'
+numNum = 2
+sumText = TextNum - numNum 
+console.log(typeof sumText);
+
+let fName = 'dsf'
+
+let fullName = !!fName
+console.log(fullName);
+//cemelcase
