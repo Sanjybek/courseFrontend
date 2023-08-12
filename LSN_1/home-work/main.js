e556e043b61643d77e48fb49983097a83d501b25
Sanjybek <sanjybek003@gmail.com>
2023-08-12 20:16:25 +0600

    home work 1

---
+++ LSN_1/home-work/main.js
@@ -0,0 +1,73 @@
+let str = 'Привет Мир!';
+console.log(str);
+
+let str1 = 'Привет';
+let str2 = ' Мир!';
+let str3 = str1 + str2;
+console.log(str3);
+console.log(str1 + str2);
+
+let name = ' Санжыбек';
+let fname = str1 + name;
+console.log(fname);
+console.log(str1 + name);
+
+let age = 20
+let year = ' лет'
+let ageYear = age + year 
+console.log('Мне ' +  ageYear);
+
+let yourТame = prompt('')
+alert(yourТame)
+
+let number = prompt()
+alert(number * number)
+
+let str4 = 'abcde';
+let array = [str4[0], str4[2], str4[4]]
+console.log(array);
+
+console.log(str4[0], str4[2], str4[4]);
+
+let a = str4[0]
+
+let c = str4[2]
+
+let e = str4[4]
+console.log(a, c, e);
+
+let num = 12345
+console.log(num[0] * num [1] * num[2] * num[3]);
+
+let seconds = 60
+let minutes = 60 
+
+let hour = seconds * minutes 
+console.log(hour); 
+ 
+let dey = hour * 24 
+console.log(dey);
+
+let month = dey * 30
+console.log(month);
+
+let hh = 4
+let mm = 23
+let ss = 30
+console.log(hh + ':' + mm + ':' + ss);
+
+let num4 = 9 
+alert(num4 * num4)
+
+var num1 = 47;
+num1 += 7;
+num1 -= 18;
+num1 *= 10;
+num1 /=15;
+alert(num1);
+
+var num2 = 10;
+num2++;
+num2++;
+num2--;
+alert(num2);
