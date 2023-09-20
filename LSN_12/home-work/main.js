50b87364268cce527fe9b66d9bf4f201f355834d
Sanjybek <sanjybek003@gmail.com>
2023-09-20 17:07:19 +0600

    home work 12

---
+++ LSN_12/home-work/main.js
@@ -0,0 +1,45 @@
+const firstValue = document.getElementById('first_value')
+const secondValue = document.getElementById('second_value')
+const result = document.getElementById('result')
+const errorMessage = document.getElementById('error')
+const ok_Btn = document.getElementById('ok')
+const opValue = (operator) => {
+        if(operator === '+'  && firstValue.value !== '' && secondValue.value !== '') {
+            const res = +firstValue.value + +secondValue.value 
+            result.value = res  
+        } else if (operator === '-' && firstValue.value && secondValue.value) {
+            const res = firstValue.value - secondValue.value 
+            result.value = res
+        } else if (operator === '/' && firstValue.value && secondValue.value) {
+            const res = firstValue.value / secondValue.value
+            result.value = res
+        } else if(operator === '*' && firstValue.value && secondValue.value) {
+            const res = firstValue.value * secondValue.value
+            result.value = res
+        } else {
+                errorMessage.innerText = 'Введите числа'
+                ok_Btn.style.display = 'flex'
+                errorMessage.style.display = 'flex'      
+        } 
+}
+ok_Btn.addEventListener('click', () => {
+    errorMessage.style.display = 'none'
+    ok_Btn.style.display = 'none'
+})
+const clearInputs = () => {
+    firstValue.value = ''
+    secondValue.value = ''
+    result.value = ''
+}
+const root = document.getElementById('root')
+const btn = document.getElementById('calculator')
+const closeBtn = document.getElementById('close')
+btn.addEventListener('click', () => {
+    root.style.display = 'block'
+    btn.style.display = 'none'
+})
+closeBtn.addEventListener('click', () => {
+    root.style.display = 'none'
+    btn.style.display = 'block'
+   
+})
